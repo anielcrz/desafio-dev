c8c1b9faa6adba6b52f167f519eb1b204aff7df4
Aniel Cruz <aniel.cruz_ext@interplayers.com.br>
2021-10-01 14:45:03 -0300

    Estrutura base da solução com mecanismo de upload de arquivo

---
+++ src/Normalize/API/Controllers/FileProcessController.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("api/file")]
+    public class FileProcessController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            try
+            {
+                var result = new List<FileUpload>();
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", file.FileName);
+                var stream = new FileStream(path, FileMode.Create);
+                await file.CopyToAsync(stream);
+                result.Add(new FileUpload() { Name = file.FileName, Length = file.Length });
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+
+    }
+}
---
+++ src/Normalize/Domain/Models/FileUpload.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Domain.Models
+{
+    public class FileUpload
+    {
+        public long Length { get; set; }
+        public string Name { get; set; }
+    }
+}
